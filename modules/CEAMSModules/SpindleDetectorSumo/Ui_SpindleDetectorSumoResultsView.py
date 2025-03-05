17eed2722a4444bd93a946fba887d8bcb22de574
boshrakh95 <boshra.khajehpiri.1@ens.etsmtl.ca>
2025-03-05 14:51:57 -0500

    Add SUMO spindle detector

---
+++ modules/CEAMSModules/SpindleDetectorSumo/Ui_SpindleDetectorSumoResultsView.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'Ui_SpindleDetectorSumoResultsView.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+import themes_rc
+
+class Ui_SpindleDetectorSumoResultsView(object):
+    def setupUi(self, SpindleDetectorSumoResultsView):
+        if not SpindleDetectorSumoResultsView.objectName():
+            SpindleDetectorSumoResultsView.setObjectName(u"SpindleDetectorSumoResultsView")
+        SpindleDetectorSumoResultsView.setStyleSheet(u"font: 12pt \"Roboto\";")
+        SpindleDetectorSumoResultsView.resize(483, 360)
+        self.verticalLayout = QVBoxLayout(SpindleDetectorSumoResultsView)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.result_layout = QVBoxLayout()
+        self.result_layout.setObjectName(u"result_layout")
+
+        self.verticalLayout.addLayout(self.result_layout)
+
+
+        self.retranslateUi(SpindleDetectorSumoResultsView)
+
+        QMetaObject.connectSlotsByName(SpindleDetectorSumoResultsView)
+    # setupUi
+
+    def retranslateUi(self, SpindleDetectorSumoResultsView):
+        SpindleDetectorSumoResultsView.setWindowTitle(QCoreApplication.translate("SpindleDetectorSumoResultsView", u"Form", None))
+    # retranslateUi
+
