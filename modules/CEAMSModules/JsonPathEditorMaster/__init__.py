2d4f20e1c06c23b7febbcd9df86ddd0a3a625bf6
mhazad99 <mhazad1999@gmail.com>
2025-05-13 15:05:12 -0400

    Json Path Editor removed temporarily for the new release

--- modules/CEAMSModules/JsonPathEditorMaster/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-@ Valorisation Recherche HSCM, Societe en Commandite – 2025
-See the file LICENCE for full license details.
-"""
-from .JsonPathEditorMaster import JsonPathEditorMaster
-from .JsonPathEditorMasterSettingsView import JsonPathEditorMasterSettingsView
-from .JsonPathEditorMasterResultsView import JsonPathEditorMasterResultsView
-from .Ui_JsonPathEditorMasterResultsView import Ui_JsonPathEditorMasterResultsView
-from .Ui_JsonPathEditorMasterSettingsView import Ui_JsonPathEditorMasterSettingsView
